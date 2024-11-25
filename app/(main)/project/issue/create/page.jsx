79efe961071679c5f2fed3840b8387f728004bde
Santosh Kumar Sah <Ssah5803@gmail.com>
2024-11-25 11:06:30 +0530

    script fixed

--- app/(main)/project/issue/create/page.jsx
+++ app/(main)/project/issue/create/page.jsx
@@ -1,6 +1,7 @@
+/** @format */
 
 "use client";
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 
 import {
   Select,
@@ -123,120 +124,139 @@ const CreateIssue = () => {
   };
 
   return (
-    <div className="container">
-      {(usersLoading || createIssueLoading) && (
-        <BarLoader width={"100%"} color="#36d7b7" />
-      )}
-      <form className="p-4 space-y-4" onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="title" className="block text-sm font-medium mb-1">
-            Title
-          </label>
-          <Input id="title" {...register("title")} placeholder="Enter title" />
-          {errors?.title && (
-            <p className="text-red-500 text-sm mt-1">{error?.title?.message}</p>
+    <Suspense fallback={<p>Loading..</p>}>
+      <div className="container">
+        {(usersLoading || createIssueLoading) && (
+          <BarLoader width={"100%"} color="#36d7b7" />
+        )}
+        <form className="p-4 space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor="title" className="block text-sm font-medium mb-1">
+              Title
+            </label>
+            <Input
+              id="title"
+              {...register("title")}
+              placeholder="Enter title"
+            />
+            {errors?.title && (
+              <p className="text-red-500 text-sm mt-1">
+                {error?.title?.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="assigneeId"
+              className="block text-sm font-medium mb-1"
+            >
+              Assignee
+            </label>
+            <Controller
+              name="assigneeId"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select Assignee" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {users?.map((user) => (
+                        <SelectItem key={user.id} value={user.id}>
+                          {user?.name}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                );
+              }}
+            />
+            {errors?.assigneeId && (
+              <p className="text-red-500 text-sm mt-1">
+                {error?.assigneeId?.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="description"
+              className="block text-sm font-medium mb-1"
+            >
+              Description
+            </label>
+            <Controller
+              name="description"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <MDEditor value={field.value} onChange={field.onChange} />
+                );
+              }}
+            />
+            {errors.description && (
+              <p className="text-red-500 text-sm mt-1">
+                {error?.description?.message}
+              </p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="priority"
+              className="block text-sm font-medium mb-1"
+            >
+              Priority
+            </label>
+            <Controller
+              name="priority"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select priority" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="LOW">LOW</SelectItem>
+                      <SelectItem value="MEDIUM">MEDIUM</SelectItem>
+                      <SelectItem value="HIGH">HIGH</SelectItem>
+                      <SelectItem value="URGENT">URGENT</SelectItem>
+                    </SelectContent>
+                  </Select>
+                );
+              }}
+            />
+            {errors.priority && (
+              <p className="text-red-500 text-sm mt-1">
+                {error?.priority?.message}
+              </p>
+            )}
+          </div>
+          {error && (
+            <p className="text-red-500 text-sm mt-1">{error.message}</p>
           )}
-        </div>
-        <div>
-          <label
-            htmlFor="assigneeId"
-            className="block text-sm font-medium mb-1"
+          <Button
+            type="submit"
+            className="w-full"
+            disabled={createIssueLoading}
           >
-            Assignee
-          </label>
-          <Controller
-            name="assigneeId"
-            control={control}
-            render={({ field }) => {
-              return (
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Select Assignee" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {users?.map((user) => (
-                      <SelectItem key={user.id} value={user.id}>
-                        {user?.name}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              );
-            }}
-          />
-          {errors?.assigneeId && (
-            <p className="text-red-500 text-sm mt-1">
-              {error?.assigneeId?.message}
-            </p>
-          )}
-        </div>
-        <div>
-          <label
-            htmlFor="description"
-            className="block text-sm font-medium mb-1"
-          >
-            Description
-          </label>
-          <Controller
-            name="description"
-            control={control}
-            render={({ field }) => {
-              return <MDEditor value={field.value} onChange={field.onChange} />;
-            }}
-          />
-          {errors.description && (
-            <p className="text-red-500 text-sm mt-1">
-              {error?.description?.message}
-            </p>
-          )}
-        </div>
-        <div>
-          <label htmlFor="priority" className="block text-sm font-medium mb-1">
-            Priority
-          </label>
-          <Controller
-            name="priority"
-            control={control}
-            render={({ field }) => {
-              return (
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="Select priority" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="LOW">LOW</SelectItem>
-                    <SelectItem value="MEDIUM">MEDIUM</SelectItem>
-                    <SelectItem value="HIGH">HIGH</SelectItem>
-                    <SelectItem value="URGENT">URGENT</SelectItem>
-                  </SelectContent>
-                </Select>
-              );
-            }}
-          />
-          {errors.priority && (
-            <p className="text-red-500 text-sm mt-1">
-              {error?.priority?.message}
-            </p>
-          )}
-        </div>
-        {error && <p className="text-red-500 text-sm mt-1">{error.message}</p>}
-        <Button type="submit" className="w-full" disabled={createIssueLoading}>
-          {createIssueLoading
-            ? issueId
-              ? "Updating..."
-              : "Creating..."
-            : issueId
-              ? "Update Issue"
-              : "Create Issue"}
-        </Button>
-      </form>
-    </div>
+            {createIssueLoading
+              ? issueId
+                ? "Updating..."
+                : "Creating..."
+              : issueId
+                ? "Update Issue"
+                : "Create Issue"}
+          </Button>
+        </form>
+      </div>
+    </Suspense>
   );
 };
 
