556fecfe341a6bfc7c6db1e560abe12a0e06f985
Santosh Kumar Sah <Ssah5803@gmail.com>
2024-11-25 15:37:21 +0530

    final commit

--- app/(main)/project/[projectId]/page.jsx
+++ app/(main)/project/[projectId]/page.jsx
@@ -3,7 +3,7 @@
 import { getProject } from "@/app/actions/projects";
 import SprintBoard from "@/components/SprintBoard";
 import SprintCreationForm from "@/components/SprintCreationForm";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import React, { useCallback, useEffect, useState } from "react";
 import { BarLoader } from "react-spinners";
 
@@ -12,22 +12,25 @@ const Project = () => {
   const { projectId } = params;
   const [projectDetails, setProjectDetails] = useState(null);
   const [loading, setLoading] = useState(false);
-
+  const router = useRouter();
   const getProjectDetails = useCallback(async () => {
     setLoading(true);
     try {
       const response = await getProject(projectId);
+      if (!response.ok) {
+        router.push("/organization/" + orgId);
+      }
       setProjectDetails(response);
     } catch (error) {
       console.log(error);
     } finally {
       setLoading(false);
     }
-  }, [projectId]);
+  }, [projectId, router]);
 
   useEffect(() => {
     getProjectDetails();
-  }, [getProjectDetails, projectId]);
+  }, [getProjectDetails, projectId, router]);
 
   return (
     <div className="container mx-auto">
